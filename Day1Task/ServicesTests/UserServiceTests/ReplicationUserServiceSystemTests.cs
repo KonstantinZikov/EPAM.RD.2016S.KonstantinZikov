c916a5887f1cf148ed878cb1b96d093e06d28396
Kanstantsin_Zykau <Kanstantsin_Zykau@EPBYMINW1861>
2016-07-14 20:13:54 +0300

    ReplicationUserServiceSystem done. ReplicationUserServiceSystem tests added.

---
+++ Day1Task/ServicesTests/UserServiceTests/ReplicationUserServiceSystemTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepositoryInterfaces;
+using ServiceInterfaces;
+using Services;
+
+namespace ServicesTests
+{
+    [TestClass]
+    public class ReplicationUserServiceSystemTests : BaseUserServiceTests
+    {
+        protected override IUserService GetService(IUserRepository repository)
+        {
+            var distributor = new DefaultUserServiceDistributor();
+            return new ReplicationUserServiceSystem(repository,distributor,"default");
+        }
+    }
+}
